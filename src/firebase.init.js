76139694a76ca3712ac49875cd6077f79e4ffe33
sharifularman <sharifulislamarman20@gmail.com>
2022-04-07 22:49:01 +0600

    firebase added

---
+++ src/firebase.init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
